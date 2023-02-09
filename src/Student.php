3df9bb13e25da3be6c44dde63d616d08536a1403
Thant Zin Oo <thantzin.oo@brycenmyanmar.com.mm>
2023-02-09 10:11:32 +0630

    edit model

--- src/Student.php
+++ src/Student.php
@@ -5,7 +5,7 @@ namespace LaravelSharedModels\SharedModels;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Blog extends Model
+class Student extends Model
 {
     use HasFactory;
 }
